1be36a41bebb23b4ac27c9600a3b9c892403b3f0
Mateo Cardona <macardonama@gmail.com>
2026-06-23 07:55:28 -0500

    Initial Commit

---
+++ PPDA/PPDAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PPDAApp.swift
+//  PPDA
+//
+//  Created by Mateo Cardona Marin on 23/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PPDAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
